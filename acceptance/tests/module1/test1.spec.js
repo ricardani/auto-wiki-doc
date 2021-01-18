404ae8628f1b7769882475767fdbb993365de248
Ricardo Silva <ricardani@gmail.com>
2021-01-18 16:31:41 +0000

    update docs

--- acceptance/tests/module1/test1.spec.js
+++ acceptance/tests/module1/test1.spec.js
@@ -18,5 +18,8 @@ describe('Name of the group', () => {
         it('should expect 2.2', () => {
             
         });
+        it('should expect 2.3', () => {
+            
+        });
     });
 });
---
+++ acceptance/tests/module2/test1.spec.js
@@ -0,0 +1,34 @@
+/*
+    Pre-Conditions:
+    User role: Admin
+    Something
+    Other
+*/
+describe('Name of the group 2', () => {
+    describe('step1', () => {
+        it('should expect 1', () => {
+            
+        });
+        describe('step2', () => {
+            it('should expect 2.1', () => {
+                
+            });
+            it('should expect 2.2', () => {
+                
+            });
+            it('should expect 2.3', () => {
+                
+            });
+            describe('step3', () => {
+                it('should expect 3.1', () => {
+                    
+                });
+                it('should expect 3.2', () => {
+                    
+                });
+            });
+        });
+    });
+
+    
+});
