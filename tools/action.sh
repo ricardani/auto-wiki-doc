80fe0187e54027687b41d3323e5815d19b1d50f4
Ricardo Silva <ricardani@gmail.com>
2021-04-06 12:27:40 +0100

    Test new wiki upload action

--- tools/action.sh
+++ tools/action.sh
@@ -3,8 +3,8 @@
 WIKIP="https://${INPUT_ACCESS_TOKEN}@github.com/${GITHUB_REPOSITORY}.wiki.git"
 
 echo "Cloning WIKI Repo..."
-git clone $WIKIP /wiki
-cd /wiki
+git clone $WIKIP ~/wiki
+cd ~/wiki
 
 echo "Cleaning..."
 rm -r *
@@ -17,7 +17,7 @@ if [ ! -d "/github/workspace/${INPUT_WIKI_FOLDER}" ]; then
     echo "Specified Wiki Folder Missing"
     exit 1
 fi
-cp -a ${INPUT_WIKI_FOLDER}/. /wiki
+cp -a ${INPUT_WIKI_FOLDER}/. ~/wiki
 
 echo "Git Config..."
 echo "-> User: ${INPUT_COMMIT_USERNAME}"
@@ -27,7 +27,7 @@ git config --global user.name "${INPUT_COMMIT_USERNAME}"
 
 echo "Commit..."
 echo "-> Message: ${INPUT_COMMIT_MESSAGE}"
-cd /wiki
+cd ~/wiki
 git add -A
 git commit -m "${INPUT_COMMIT_MESSAGE}"
 git push $WIKIP
