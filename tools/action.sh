c13ab572962af431d3fe4f065008c93142526465
Ricardo Silva <ricardani@gmail.com>
2021-04-06 12:43:00 +0100

    Test new wiki upload action

--- tools/action.sh
+++ tools/action.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-CURRENT_FOLDER=${pwd}
+CURRENT_FOLDER=$(pwd)
 WIKIP="https://${INPUT_ACCESS_TOKEN}@github.com/${GITHUB_REPOSITORY}.wiki.git"
 
 echo "Cloning WIKI Repo..."
@@ -22,14 +22,11 @@ cp -a ${INPUT_WIKI_FOLDER}/. ~/wiki-tmp
 echo "Git Config..."
 echo "-> User: ${INPUT_COMMIT_USERNAME}"
 echo "-> Email: ${INPUT_COMMIT_EMAIL}"
-git config --global user.email "${INPUT_COMMIT_EMAIL}"
-git config --global user.name "${INPUT_COMMIT_USERNAME}"
-
 echo "Commit..."
 echo "-> Message: ${INPUT_COMMIT_MESSAGE}"
 cd ~/wiki-tmp
 git add -A
-git commit -m "${INPUT_COMMIT_MESSAGE}"
+git -c user.name="${INPUT_COMMIT_USERNAME}" -c user.email="${INPUT_COMMIT_EMAIL}" commit -m "${INPUT_COMMIT_MESSAGE}"
 git push $WIKIP
 
 cd $CURRENT_FOLDER
