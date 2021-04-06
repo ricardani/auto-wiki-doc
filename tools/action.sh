0d8f1473d1945ac39e7ee2d5536564ce580a0782
Ricardo Silva <ricardani@gmail.com>
2021-04-06 12:47:43 +0100

    Test new wiki upload action

--- tools/action.sh
+++ tools/action.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 MAIN_FOLDER=$(pwd)
-WIKIP="https://${INPUT_ACCESS_TOKEN}@github.com/${GITHUB_REPOSITORY}.wiki.git"
+WIKIP="https://${ACCESS_TOKEN}@github.com/${GITHUB_REPOSITORY}.wiki.git"
 
 echo "Cloning WIKI Repo..."
 git clone $WIKIP ~/wiki-tmp
@@ -9,22 +9,22 @@ echo "Cleaning..."
 rm -r ~/wiki-tmp/*
 
 echo "Copy Files..."
-echo "-> Wiki Folder: ${INPUT_WIKI_FOLDER}"
+echo "-> Wiki Folder: ${WIKI_FOLDER}"
 
-if [ ! -d "./${INPUT_WIKI_FOLDER}" ]; then
+if [ ! -d "./${WIKI_FOLDER}" ]; then
     echo "Specified Wiki Folder Missing"
     exit 1
 fi
-cp -a ${INPUT_WIKI_FOLDER}/. ~/wiki-tmp
+cp -a ${WIKI_FOLDER}/. ~/wiki-tmp
 
 echo "Git Config..."
-echo "-> User: ${INPUT_COMMIT_USERNAME}"
-echo "-> Email: ${INPUT_COMMIT_EMAIL}"
+echo "-> User: ${COMMIT_USERNAME}"
+echo "-> Email: ${COMMIT_EMAIL}"
 echo "Commit..."
-echo "-> Message: ${INPUT_COMMIT_MESSAGE}"
+echo "-> Message: ${COMMIT_MESSAGE}"
 cd ~/wiki-tmp
 git add -A
-git -c user.name="${INPUT_COMMIT_USERNAME}" -c user.email="${INPUT_COMMIT_EMAIL}" commit -m "${INPUT_COMMIT_MESSAGE}"
+git -c user.name="${COMMIT_USERNAME}" -c user.email="${COMMIT_EMAIL}" commit -m "${COMMIT_MESSAGE}"
 git push $WIKIP
 
 echo "Moving to the main folder..."
