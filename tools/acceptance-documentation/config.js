d2db744d1b77654e81a56a5cde0d78a177280c37
Ricardo Silva <ricardani@gmail.com>
2021-04-06 12:02:08 +0100

    Test new wiki upload action

--- tools/acceptance-documentation/config.js
+++ tools/acceptance-documentation/config.js
@@ -1,7 +1,7 @@
 const ACCEPTANCE_TEST_SRC_FOLDER = 'acceptance/tests';
 const WDIO_REPORT_FOLDER = 'reports/json';
-const DOCS_ACCEPTANCE_FOLDER = 'wiki/acceptance-tests';
-const DOCS_FOLDER = 'wiki';
+const DOCS_ACCEPTANCE_FOLDER = 'docs/acceptance-tests';
+const DOCS_FOLDER = 'docs';
 const WIKI_URL = 'https://github.com/ricardani/auto-wiki-doc/wiki';
 
 module.exports = {
